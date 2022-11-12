6a970e3ff615dbdb91630de19ab968bee6600e53
Gabriel Assys <brachak2001@gmail.com>
2022-11-12 13:24:30 -0300

    Adicionado local storage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+console.log(itens)
+
+itens.forEach((elemento) => 
+{
+    console.log(elemento.nome, elemento.quantidade)
+})
 
 form.addEventListener("submit", (evento) => 
 {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome'] 
+    const quantidade = evento.target.elements['quantidade']
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    criaElemento(nome.value, quantidade.value)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade) 
